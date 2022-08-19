5e41a921cbbbfe95585795064121d0a8f7542073
Tan Dung <tandung12072002@gmail.com>
2022-08-20 00:20:59 +0700

    kanji page

--- frontend_reactjs/src/pages/Kanji/Kanji.js
+++ frontend_reactjs/src/pages/Kanji/Kanji.js
@@ -1,18 +1,48 @@
 import classNames from "classnames/bind";
 import styles from "./Kanji.module.scss";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import request from "~/utils/request";
 const cx = classNames.bind(styles);
 
 function Kanji() {
-  // console.log(props.match)
-  // const [Kanji, setKanji] = useState();
-  // useEffect(() => {
-  //   fetch(`http://127.0.0.1:8000/api/kanji/1`)
-  //     .then((res) => res.json())
-  //     .then((res) => {
-  //       setKanji(res.Kanji);
-  //     });
-  // }, []);
-  return <div>Kanji</div>;
+  const { id } = useParams();
+  const [kanji, setKanji] = useState();
+  useEffect(() => {
+    request.get(`kanji/${id}`).then((res) => {
+      setKanji(res.data.Kanji);
+    });
+  }, []);
+  return (
+    <>
+      {kanji ? (
+        <div className={cx("card")}>
+          <div className={cx("card_header", `${kanji.type}`)}>
+            <div className={cx("card_title")}>{kanji.title}</div>
+          </div>
+          <div className={cx("card_content")}>
+            <div className={cx("box", `${kanji.type}`)}>
+              <span>Ý nghĩa</span>
+            </div>
+            <p className={cx("text")}>{kanji.mean}</p>
+          </div>
+          <div className={cx("card_content")}>
+            <div className={cx("box", `${kanji.type}`)}>
+              <span>Cấu trúc</span>
+            </div>
+            <p className={cx("text")}>{kanji.structure.split(";")} </p>
+          </div>
+          <div className={cx("card_content")}>
+            <div className={cx("box", "ex", `${kanji.type}`)}>
+              <span>Ví dụ</span>
+            </div>
+            <p className={cx("text")}>{kanji.example}</p>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
 }
 export default Kanji;
