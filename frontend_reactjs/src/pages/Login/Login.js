d06def87caa415c85aa29220653a5da32916092a
Tan Dung <tandung12072002@gmail.com>
2022-08-24 16:02:54 +0700

    register

--- frontend_reactjs/src/pages/Login/Login.js
+++ frontend_reactjs/src/pages/Login/Login.js
@@ -3,7 +3,7 @@ import classNames from "classnames/bind";
 import Button from "~/components/Button/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faGoogle } from "@fortawesome/free-brands-svg-icons";
-import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
+import { faLock, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import Signup from "../Signup/Signup";
 import { useState } from "react";
 
@@ -16,67 +16,65 @@ function Login() {
     setCheckClickBtnSignup(true);
   };
   return (
-    <>
-      {checkClickBtnSignup ? (
-        <Signup></Signup>
-      ) : (
-        <div>
-          <div className={cx("background")}>
-            <div className={cx("shape")}></div>
-            <div className={cx("shape")}></div>
-          </div>
-          <div className={cx("background")}>
-            <div className={cx("shape")}></div>
-            <div className={cx("shape")}></div>
-          </div>
-          <form className={cx("formLogin")}>
-            <img className={cx("logo")} src="/img/logo2.png" alt="logo" />
-            <h3>LOGIN</h3>
-            <label for="username">
-              <FontAwesomeIcon
-                icon={faUser}
-                className={cx("icon")}
-              ></FontAwesomeIcon>
-              Username
-            </label>
-            <input type="text" placeholder="Email" id="username" />
-            <label for="password">
-              <FontAwesomeIcon
-                icon={faLock}
-                className={cx("icon")}
-              ></FontAwesomeIcon>
-              Password
-            </label>
-            <input type="password" placeholder="Password" id="password" />
+    <div>
+      <div className={cx("background")}>
+        <div className={cx("shape")}></div>
+        <div className={cx("shape")}></div>
+      </div>
+      <div className={cx("background")}>
+        <div className={cx("shape")}></div>
+        <div className={cx("shape")}></div>
+      </div>
+      <form className={cx("formLogin")}>
+        <img className={cx("logo")} src="/img/logo2.png" alt="logo" />
+        <h3>LOGIN</h3>
+        <label>
+          <FontAwesomeIcon
+            icon={faEnvelope}
+            className={cx("icon")}
+          ></FontAwesomeIcon>
+          Email
+        </label>
+        <input type="text" placeholder="Email" />
+        <label>
+          <FontAwesomeIcon
+            icon={faLock}
+            className={cx("icon")}
+          ></FontAwesomeIcon>
+          Password
+        </label>
+        <input type="password" placeholder="Password" />
 
-            <p className={cx("signup")} onClick={handleClickSignup}>
-              {" "}
-              Signup
-            </p>
-            <Button primary className={cx("btnLogin")}>
-              Log In
-            </Button>
+        <p className={cx("signup")} onClick={handleClickSignup}>
+          Signup
+        </p>
+        <Button primary className={cx("btnLogin")}>
+          Log In
+        </Button>
 
-            <div className={cx("social")}>
-              <div className={cx("go")}>
-                <FontAwesomeIcon
-                  icon={faGoogle}
-                  className={cx("icon")}
-                ></FontAwesomeIcon>
-                Google
-              </div>
-              <div className={cx("fb")}>
-                <FontAwesomeIcon
-                  icon={faFacebook}
-                  className={cx("icon")}
-                ></FontAwesomeIcon>
-                Facebook
-              </div>
-            </div>
-          </form>
+        <div className={cx("social")}>
+          <div className={cx("go")}>
+            <FontAwesomeIcon
+              icon={faGoogle}
+              className={cx("icon")}
+            ></FontAwesomeIcon>
+            Google
+          </div>
+          <div className={cx("fb")}>
+            <FontAwesomeIcon
+              icon={faFacebook}
+              className={cx("icon")}
+            ></FontAwesomeIcon>
+            Facebook
+          </div>
         </div>
+      </form>
+      {checkClickBtnSignup ? (
+        <Signup className="active"></Signup>
+      ) : (
+        <Signup></Signup>
       )}
-    </>
+    </div>
   );
 }
 
--- frontend_reactjs/src/pages/Signup/Signup.js
+++ frontend_reactjs/src/pages/Signup/Signup.js
@@ -1,10 +1,38 @@
 import styles from "./Signup.module.scss";
 import classNames from "classnames/bind";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faLock, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import Button from "~/components/Button/Button";
+import { useState } from "react";
 const cx = classNames.bind(styles);
 
-function Signup() {
-  return <div className={cx("formSignup")}>Signup</div>;
+function Signup({ className }) {
+  const [classNames, setClassName] = useState(className);
+  return (
+    <div className={cx("container", classNames)}>
+      <div className={cx("formSignup")}>
+        <h2>Create your account</h2>
+        <form>
+          <input type="text" placeholder="Username:" required />
+          <input type="text" placeholder="Email:" required />
+          <input type="password" placeholder="Password:" required />
+          <input type="password" placeholder="Repeat password:" required />
+          <Button outline className={cx("btnRegister")}>
+            Register
+          </Button>
+        </form>
+        <button
+          className={cx("btnBack")}
+          onClick={() => {
+            setClassName("");
+          }}
+        >
+          <FontAwesomeIcon icon={faArrowLeft}></FontAwesomeIcon>
+        </button>
+      </div>
+      <div className={cx("img")}></div>
+    </div>
+  );
 }
 
 export default Signup;
